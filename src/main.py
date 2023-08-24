726947150cf1f77b1ce3ea52721276e600ac07a9
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-24 21:53:01 +0300

    В main основной код реализован в виде функции main()

--- src/main.py
+++ src/main.py
@@ -1,27 +1,33 @@
-from src.utils import load_operations, sorted_operations_list_by_datetime, formatted_the_operation
+from src.utils import load_operations, sorted_operations_list_by_datetime, formatted_the_operation, get_executed_only
 import pathlib
-from pathlib import Path
 
 # Загруженный список операций в формате JSON
-PATH = Path(pathlib.Path.cwd(), "operations.json")
+PATH = pathlib.Path(pathlib.Path.cwd(), "operations.json")
 
-# Список операций переведенный из JSON
-operations = load_operations(PATH)
+
 
 # Отсортированный по дате и времени список операций
-sorted_operations_by_date = sorted_operations_list_by_datetime(operations)
+
 
 # Счетчик выводимых операций на экран
 count_operations = 0
 
-# Цикл вывода на экран последних 5 выполненных операций
-for operation in sorted_operations_by_date:
-    if count_operations > 5:
-        break
 
-    if operation['state'] == 'CANCELED':
-        count_operations += 1
-        continue
-    elif operation['state'] == 'EXECUTED':
-        count_operations += 1
+def main():
+    """
+    Функция вывода на экран последних 5 выполненных операций
+    """
+    # Список операций переведенный из JSON
+    operations = load_operations(PATH)
+    # Отсортированный по дате и времени список операций
+    sorted_operations_by_date = sorted_operations_list_by_datetime(operations)
+    # 5 последних удачных операций
+    five_executed_operations = get_executed_only(sorted_operations_by_date, 5)
+
+    # Цикл вывода на экран удачных операций
+    for operation in five_executed_operations:
         print(formatted_the_operation(operation))
+
+
+if __name__ == '__main__':
+    main()
